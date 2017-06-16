fd25547293d2478934a08bf60b38f090345b5ea4
JINGLUO <leojing1989@gmail.com>
2017-06-17 00:55:50 +1000

    refine Unit Test

--- MazeTests/MazeTests.swift
+++ MazeTests/MazeTests.swift
@@ -12,50 +12,47 @@ import XCTest
 class MazeTests: XCTestCase {
   
   var vc = ViewController()
-
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-      
-      let storyboard = UIStoryboard(name: "Main", bundle: Bundle.main)
-      vc = storyboard.instantiateInitialViewController() as! ViewController
-    }
+  
+  override func setUp() {
+    super.setUp()
+    // Put setup code here. This method is called before the invocation of each test method in the class.
     
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
+    let storyboard = UIStoryboard(name: "Main", bundle: Bundle.main)
+    vc = storyboard.instantiateInitialViewController() as! ViewController
+  }
+  
+  override func tearDown() {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    super.tearDown()
+  }
+  
+  func testIfRevisitRoom() {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
     
-    func testIfRevisitRoom() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    XCTAssertNotNil(vc.mazeLogicManager)
+    
+    for i in 0 ..< 10 {
+      let ex = self.expectation(description: String(i))
       
-      XCTAssertNotNil(vc.mazeLogicManager)
+      self.vc.mazeLogicManager.startFetchRoom(at: (x: 0, y: 0))
       
-      let startRooms = [(10, "R40912fa5dc6cdd6d"), (10, "R469737bdda6ac575"), (10, "R4b9a35bfd767c777"), (10, "R76a73fb5ea5acd7d"), (10, "R5b8a2fa5c777dd6d"), (10, "R419036bcdd6dc474"), (10, "R5a8b21abc676d363")]
-      for (t, roomId) in startRooms {
-        let ex = self.expectation(description: roomId)
-        
-        self.vc.mazeLogicManager.visitedRooms = []
-        self.vc.mazeLogicManager.traversalRooms(roomId, start: (0,0))
-        
-        print(roomId)
-        sleep(UInt32(t))
-        ex.fulfill()
-        
-        //Async function when finished call [expectation fullfill]
-        self.waitForExpectations(timeout: 0) { (error) in
-          XCTAssertNotNil(self.vc.mazeLogicManager.visitedRooms, "rooms are not empty")
-          XCTAssertFalse(self.vc.mazeLogicManager.roomVisitedTwice())
-        }
+      sleep(10)
+      ex.fulfill()
+      
+      //Async function when finished call [expectation fullfill]
+      self.waitForExpectations(timeout: 0) { (error) in
+        XCTAssertNotNil(self.vc.mazeLogicManager.visitedRooms, "rooms are not empty")
+        XCTAssertFalse(self.vc.mazeLogicManager.roomVisitedTwice())
       }
     }
+  }
   
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+  func testPerformanceExample() {
+    // This is an example of a performance test case.
+    self.measure {
+      // Put the code you want to measure the time of here.
     }
-    
+  }
+  
 }
